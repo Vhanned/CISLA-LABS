660a111e70aa81f7e298b9943ebfce8a434dd273
Ivan Argandoña <138079469+Vhanned@users.noreply.github.com>
2025-11-26 21:04:51 -0600

    Formulario funcional
    Dentro de un tiempo se realizara el cambio de funcionalidad a que el formulario envie efectivamente un correo en lugar de guardarse el mensaje en una base de datos.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), 
-    provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient()
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDQMytBDHLWRk2C60At5glOIWGiyVPQi3Y",
+        authDomain: "cislalabs.firebaseapp.com",
+        projectId: "cislalabs",
+        storageBucket: "cislalabs.firebasestorage.app",
+        messagingSenderId: "380477945024",
+        appId: "1:380477945024:web:97c48d61c760de575fa925",
+        measurementId: "G-5BZFR6KXWP"
+    }
+};
